5622222ff8edee8b1a0d14cbeb78ccbf22c450c6
jean conceição <89525535+jeansanto@users.noreply.github.com>
2022-09-13 14:35:17 -0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -5,9 +5,12 @@ function sum(v1, v2) {}
 function sub(v1, v2) {}
 
 // retorna o valor da divisão entre os dois parâmetros
-function div(v1, v2) {
-    return v1/v2
+function dividir(v1, v2) {
+    return v1 / v2;
 }
+
+const res = dividir(14, 50);
+console.log(res);
 
 // retorna o valor da multiplicação entre os dois parâmetros
 function mult(v1, v2) {}
