a4b2372a15a202e9e2ecfff45d730883df642c8c
Kaleu farias <kaleufarias96@gmail.com>
2022-09-13 11:43:15 -0300

    :sparkles: multiplicaçao

--- calculator.js
+++ calculator.js
@@ -7,8 +7,20 @@ function sub(v1, v2) {}
 // retorna o valor da divisão entre os dois parâmetros
 function div(v1, v2) {}
 
-// retorna o valor da multiplicação entre os dois parâmetros
-function mult(v1, v2) {}
+// Kaleu retorna o valor da multiplicação entre os dois parâmetros
+function mult(a, b) {
+    if(a < b){
+        return mult(b, a);
+    }else if (b != 0){
+        return a + mult(a, b - 1);
+    }else{
+        return 0;
+    }
+}
+console.log(mult(10, 5));
+console.log(mult(7, 6));
+console.log(mult(8, 3));
+
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
