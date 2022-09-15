1d1d6b733fed7f31beb68ef4122d1254c23a57a3
Natan Pezzini <natanlenhard@hotmail.com>
2022-09-15 20:49:14 -0300

    Adicionando funcionalidade de raiz quadrada

--- calculator.js
+++ calculator.js
@@ -13,6 +13,6 @@ function div(v1, v2) {return v1/v2}
 function mult(v1, v2) {return v1*v2}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
+function square(v1) {return v1*v1}
 
 console.log(sum(v1, v2));
