8f2872ee17ea9797ff6e10443e21d96812c12c75
Testessz <meudrive627@gmail.com>
2022-09-17 10:40:25 -0300

    :sparkles: Adiciona Função soma

--- calculator.js
+++ calculator.js
@@ -32,8 +32,10 @@ menu:
     }
 
 function sum(){  //TODO: Implementar soma
-
-
+    let a = Number(prompt("Primerio Número: ") );
+    let b = Number(prompt("Segundo Número: ") );
+    let res = a + b;
+    console.log(`Resultado: ${res}`);
 }
 
 function sub(){  //TODO: Implementar subtração
