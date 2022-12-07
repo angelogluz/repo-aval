2af170db65a59049d5471fd275f68addffc06d28
yurisilvasoares <54381764+yurisilvasoares@users.noreply.github.com>
2022-12-07 20:37:09 -0300

    :sparkles: Subtração
    Adicionando a função de subtrair

--- calculator.js
+++ calculator.js
@@ -2,7 +2,9 @@
 function sum(v1, v2) {}
 
 // retorna o valor da subtração entre os dois parâmetros
-function sub(v1, v2) {}
+function sub(v1, v2) {
+    return v1 - v2
+}
 
 // retorna o valor da divisão entre os dois parâmetros
 function div(v1, v2) {}
@@ -13,4 +15,6 @@ function mult(v1, v2) {}
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
 
-console.log(sum(v1, v2));
+let v1 = 10
+let v2 = 5
+console.log(sub(v1, v2));
