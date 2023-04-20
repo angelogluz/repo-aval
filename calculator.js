72463007edf92a762d5ac3448f666116b288b41f
Igor Leon Borges Silva Simim <igorleonsimim@gmail.com>
2023-04-20 19:56:17 -0300

    :sparkles: Add function square

--- calculator.js
+++ calculator.js
@@ -19,9 +19,12 @@ function mult(v1, v2) {
 }
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
+function square(v1) {
+    return Math.sqrt(v1)
+}
 
 console.log(sum(5, 75));
 console.log(sub(8, 3));
 console.log(div(15, 5));
 console.log(mult(2, 5));
+console.log(square(25));
