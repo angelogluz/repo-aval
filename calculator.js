ef983eaf5d4ea6f6945f466a5935cee1fccecf81
eduardocaas <eduardofc.dev@gmail.com>
2023-04-20 10:04:01 -0300

    :white_check_mark: Implemented function square

--- calculator.js
+++ calculator.js
@@ -17,7 +17,10 @@ function div(v1, v2) {
 function mult(v1, v2) {}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
+function square(v1) {
+  return Math.sqrt(v1);
+}
 
 console.log(sum(2, 4));
 console.log(div(10,5));
+console.log(square(9));
