5632838e31f28de37d9719340d4825912b9926d7
amaralTheSage <amaraldesouza9@gmail.com>
2024-09-18 15:17:36 -0300

    🎨 Reformats code, removes unnecessary stuff

--- calculator.js
+++ calculator.js
@@ -5,28 +5,24 @@ function sum(v1, v2) {
 
 // retorna o valor da subtração entre os dois parâmetros
 function sub(v1, v2) {
-    return v1 - v2
+  return v1 - v2;
 }
 
 // retorna o valor da divisão entre os dois parâmetros
 function div(v1, v2) {
-    const divs = v1 / v2
+  const divs = v1 / v2;
 
-    return divs
+  return divs;
 }
 
 // retorna o valor da multiplicação entre os dois parâmetros
 function mult(v1, v2) {
-    return v1 * v2
-    //uwu\\
+  return v1 * v2;
 }
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {
-    const squares = Math.sqrt(v1)
+  const squares = Math.sqrt(v1);
 
-    return squares
+  return squares;
 }
-
-console.log(sum(v1, v2));
-console.log(sub(v1, v2));
