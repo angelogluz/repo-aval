7b02c324addd76435b517ff447e7a79058e60ee3
Xande025 <alexandreradmann403@gmail.com>
2024-09-21 11:40:16 -0300

    :white_check_mark: Recurso/Raiz Quad

--- calculator.js
+++ calculator.js
@@ -19,6 +19,12 @@ function mult(v1, v2) {
 }
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
+function square(v1) {
+    return Math.sqrt(v1);
+}
 
-console.log(sum(v1, v2));
+let v1 = 16;
+let v2 = 4;
+
+console.log(sum(v1, v2));    // Exemplo: 16 + 4 = 20
+console.log(square(v1));     // Exemplo: Raiz quadrada de 16 = 4
