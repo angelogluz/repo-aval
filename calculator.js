8b5717ef9ce554ab50bc65446d48c726a4d157e2
evighi <eduardovighi8@gmail.com>
2024-09-23 20:24:50 -0300

    ✨ Add console.log sum and sub

--- calculator.js
+++ calculator.js
@@ -25,7 +25,8 @@ function mult(v1, v2) {
 function square(v1) {
     return Math.sqrt(v1);
 }
-
+console.log(sum(v1, v2));
+console.log(sub(v1, v2));
 console.log(div(v1, v2));
 console.log(mult(v1, v2)); 
 console.log(square(v1));
