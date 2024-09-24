d3ac5eae8f480bfa4612d3b63d23677c9870f923
KarolineKS <kkohlsstangherlin@gmail.com>
2024-09-24 19:00:50 -0300

    :test_tube: test/testes manuais function power

--- calculator.js
+++ calculator.js
@@ -30,12 +30,22 @@ function power(base, exponent) {
 let v1 = 16;
 let v2 = 4;
 
-const base = 2;
 const exponent = 3;
+const exponent2 = 2;
+const base = 2;
+const base2 = 3;
+const base3 = 4;
+const base4 = 5;
 
 console.log(sum(v1, v2));    // Exemplo: 16 + 4 = 20
 console.log(square(v1));     // Exemplo: Raiz quadrada de 16 = 4
 console.log(sub(v1, v2)); 
 console.log(div(v1, v2)); 
 console.log(mult(v1, v2)); 
-console.log(power(base, exponent)); 
+
+//testes manuais da função power
+console.log(power(base, exponent)); // resultado esperado 8
+console.log(power(base2, exponent)); // resultado esperado 9
+console.log(power(base3, exponent2)); // resultado esperado 16
+console.log(power(base4, exponent2)); // resultado esperado 25
+console.log(power(base4, exponent)); // resultado esperado 125
